c433debcb69c07b3237f8536ac9b3d67bc20834d
Richard <richardkenyon1991@hotmail.co.uk>
2024-01-15 09:18:19 +0000

    add instructions enum and from_char in impl

---
+++ bft_types/src/lib.rs
@@ -0,0 +1,37 @@
+use std::path::Path;
+
+#[derive(Debug)]
+enum Instruction {
+    MoveLeft,
+    MoveRight,
+    Increment,
+    Decrement,
+    Input,
+    Output,
+    ConditionalJumpForward,
+    ConditionalJumpBackward,
+}
+
+impl Instruction{
+    pub fn from_char(c: char)->Option<Instruction>{
+        match c {
+            '<' => Some(Instruction::MoveLeft),
+            '>' => Some(Instruction::MoveRight),
+            '+' => Some(Instruction::Increment),
+            '-' => Some(Instruction::Decrement),
+            '.' => Some(Instruction::Input),
+            ',' => Some(Instruction::Output),
+            '[' => Some(Instruction::ConditionalJumpForward),
+            ']' => Some(Instruction::ConditionalJumpBackward),
+            _ => None
+        }
+    }
+}
+
+
+#[derive(Debug)]
+struct BfProgram{
+    name: String,
+    instructions: Vec<Instruction>
+}
+
