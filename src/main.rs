156edb6f97728d109983a3067bccd0c8e34f4590
Richard <richardkenyon1991@hotmail.co.uk>
2024-01-17 23:09:05 +0000

    start implementing the top-level main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use std::env::{self, Args};
+use std::path::PathBuf;
+use bft_types::BfProgram;
+use bft_interp::VirtualMachine;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    let file_path = PathBuf::from(env::args().nth(1).ok_or("Needs BF program file name")?);
+
+    let bf_program = BfProgram::from_file(file_path.as_path())?;
+    let bf_interpreter:VirtualMachine<u8> = VirtualMachine::new(30000, false);
+    
     Ok(())
 }
