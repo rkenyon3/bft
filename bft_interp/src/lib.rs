77ee5a3a06b39ba5c3f6bf4ceef2057a3c4b10ef
Richard <richardkenyon1991@hotmail.co.uk>
2024-01-17 22:44:41 +0000

    create struct for VirtualMachine and start on impl

--- bft_interp/src/lib.rs
+++ bft_interp/src/lib.rs
@@ -1,3 +1,21 @@
-pub fn do_nothing() {
-    println!("Potatoes")
+pub struct VirtualMachime<T> {
+    cells: Vec<T>,
+    pointer: usize,
+    tape_size: usize,
+    tape_can_grow: bool,
+}
+
+impl<T> VirtualMachime<T> {
+    pub fn new(mut tape_size: usize, tape_can_grow: bool) -> Self {
+        if tape_size == 0 {
+            tape_size = 30000;
+        }
+
+        Self {
+            cells: Vec::<T>::new(),
+            pointer: 0,
+            tape_size,
+            tape_can_grow,
+        }
+    }
 }
