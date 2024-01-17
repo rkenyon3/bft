5f06fa7156d4e43b4bb9531a71c3b5c2e3e14f0d
Richard <richardkenyon1991@hotmail.co.uk>
2024-01-17 22:46:17 +0000

    fix typo in VirtualMachine name

--- bft_interp/src/lib.rs
+++ bft_interp/src/lib.rs
@@ -1,11 +1,11 @@
-pub struct VirtualMachime<T> {
+pub struct VirtualMachine<T> {
     cells: Vec<T>,
     pointer: usize,
     tape_size: usize,
     tape_can_grow: bool,
 }
 
-impl<T> VirtualMachime<T> {
+impl<T> VirtualMachine<T> {
     pub fn new(mut tape_size: usize, tape_can_grow: bool) -> Self {
         if tape_size == 0 {
             tape_size = 30000;
