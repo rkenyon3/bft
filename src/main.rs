99e7bdfd1dd1a36b216256b9758d1d37395d8604
Richard <richardkenyon1991@hotmail.co.uk>
2024-01-17 23:12:28 +0000

    fix formatting cos cargo complained in ci

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
+use bft_interp::VirtualMachine;
+use bft_types::BfProgram;
 use std::env::{self, Args};
 use std::path::PathBuf;
-use bft_types::BfProgram;
-use bft_interp::VirtualMachine;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file_path = PathBuf::from(env::args().nth(1).ok_or("Needs BF program file name")?);
 
     let bf_program = BfProgram::from_file(file_path.as_path())?;
-    let bf_interpreter:VirtualMachine<u8> = VirtualMachine::new(30000, false);
-    
+    let bf_interpreter: VirtualMachine<u8> = VirtualMachine::new(30000, false);
+
     Ok(())
 }
