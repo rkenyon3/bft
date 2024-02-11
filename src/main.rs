9a2aa2dd0506d52380f2eeb8316401ca7cf06a8c
Richard Kenyon <richardkenyon1991@hotmail.co.uk>
2024-02-11 22:53:58 +0000

    rework main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,23 @@ use bft_types::BfProgram;
 use clap::Parser;
 
 /// Struct to hold the parameters for running the bft.
-struct BftParams{
+struct BftParams {
     program_file: PathBuf,
     tape_cell_count: Option<NonZeroUsize>,
-    tape_is_extensible: bool
+    tape_is_extensible: bool,
 }
 
-impl BftParams{
-    fn new(program_file: PathBuf, tape_cell_count: Option<NonZeroUsize>, tape_is_extensible: bool) -> Self
-    {
-        Self{program_file,tape_cell_count, tape_is_extensible}
+impl BftParams {
+    fn new(
+        program_file: PathBuf,
+        tape_cell_count: Option<NonZeroUsize>,
+        tape_is_extensible: bool,
+    ) -> Self {
+        Self {
+            program_file,
+            tape_cell_count,
+            tape_is_extensible,
+        }
     }
 }
 
@@ -27,7 +34,8 @@ fn run_bft(params: BftParams) -> Result<(), Box<dyn std::error::Error>> {
 
     bf_program.analyse_program()?;
 
-    let bf_interpreter: VirtualMachine<u8> = VirtualMachine::new(params.tape_cell_count,params.tape_is_extensible);
+    let bf_interpreter: VirtualMachine<u8> =
+        VirtualMachine::new(params.tape_cell_count, params.tape_is_extensible);
 
     bf_interpreter.interpret_program(&bf_program);
 
@@ -37,5 +45,11 @@ fn run_bft(params: BftParams) -> Result<(), Box<dyn std::error::Error>> {
 fn main() {
     let args = cli::Args::parse(); // TODO: this should really be seperated out better
 
-//    let params = BftParams::new
+    let params = BftParams::new(args.program, args.cells, args.extensible);
+
+    let run_result = run_bft(params);
+    match run_result{
+        Ok(()) => println!("Done"),
+        Err(e) => println!("Error: {}", e)
+    }
 }
